1452738d5d252d4628609ea2288c12467b8b8f33
Kondrashka87 <kondraxa@mail.ru>
2023-01-03 16:00:01 +0200

    Index_03_01

---
+++ src/test/java/Index.java
@@ -0,0 +1,88 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Index
+{
+    WebDriver wd;
+
+    @BeforeMethod
+    public void start()
+    {
+        wd = new ChromeDriver();
+        wd.get("file:///C:/Users/size7/Downloads/index.html");
+        wd.manage().window().maximize();
+    }
+
+    @Test
+    public void tableTest()
+    {
+        //proverit' pravda li v tablize 4 stroki
+        List<WebElement> rows = wd.findElements(By.cssSelector("tr"));
+        Assert.assertEquals(rows.size(), 4);
+
+        //pravda li chto poslednyaya stroka soderzit Mexico
+        WebElement lastRow = wd.findElement(By.cssSelector("tr:last-child"));
+        String text = lastRow.getText();
+        System.out.println(text);
+        //Poland Chine Mexico
+        Assert.assertTrue(text.contains("Mexico"));
+
+        //pravda li chto poslednyaya yache'ka soderzit Mexico
+        WebElement lastCell = wd.findElement(By.cssSelector("tr:last-child td:last-child"));
+        String str = lastCell.getText();
+        boolean res = str.contains("Mexico");
+        Assert.assertTrue(str.contains("Mexico"));
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void itemsTests()
+    {
+        // find Item1 & click() ===> assert that "div-alert" contains message  "Clicked by Item 1"
+        WebElement item1 = wd.findElement(By.cssSelector("li:first-child"));
+        item1.click();
+        WebElement alert = wd.findElement(By.cssSelector("#alert"));
+        String str = alert.getText();
+        Assert.assertTrue(str.contains("Clicked by Item 1"));
+
+        // find Item3 & click() ===> assert that "div-alert" contains message  "Clicked by Item 3"
+        List <WebElement> items = wd.findElements(By.cssSelector("li"));
+        WebElement item3 = items.get(2);
+        item3.click();
+        WebElement alert3 = wd.findElement(By.cssSelector("#alert"));
+        String str3 = alert.getText();
+        Assert.assertTrue(str3.contains("Clicked by Item 3"));
+    }
+
+    @Test
+    public void formTests()
+    {
+        // fill name & fill surename & click send
+        WebElement name = wd.findElement(By.cssSelector("input:first-child"));
+        name.click();
+        name.clear();
+        name.sendKeys("Anna123#");
+
+        WebElement surename = wd.findElement(By.cssSelector("input:last-of-type"));
+        surename.click();
+        surename.clear();
+        surename.sendKeys("Kondrateva");
+
+        List <WebElement> buttons = wd.findElements(By.tagName("button"));
+        WebElement send = buttons.get(0);
+        send.click();
+
+        // Assert that "div-alert" contains text with name + surename
+        WebElement alertname = wd.findElement(By.cssSelector("#alert"));
+        String strName = alertname.getText();
+        Assert.assertTrue(strName.contains("Anna123# Kondrateva"));
+    }
+
+}
--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -95,8 +95,72 @@ public class Login
         List <WebElement> buttons = wd.findElements(By.tagName("button"));
         WebElement buttonReg = buttons.get(1);
         buttonReg.click();
-        wd.quit();
     }
+    @Test
+    public void ccsLocatrstest()
+    {
+        // нужно перейти на страницу логина, так как на главной странице мало элементов для поиска и тренировки
+        // переход на страницу логина тут:
+
+        wd.findElement(By.linkText("LOGIN")).click();
+
+        // теперь мы на странице логина и тут будем тренироваться
+
+        // На странице логин найти несколько элементов по их  tagname
+        wd.findElement(By.tagName("div"));
+        wd.findElement(By.cssSelector("div"));
+        wd.findElement(By.cssSelector("a"));
+        wd.findElement(By.cssSelector("input"));
+        wd.findElement(By.cssSelector("button"));
+
+        // ON Login PAGE find some elements by class
+        wd.findElement(By.className("container"));
+        wd.findElement(By.cssSelector(".container"));
+        wd.findElement(By.cssSelector(".active")); //class 'active'
+        // ili
+        wd.findElement(By.cssSelector("[class = 'active']"));
+
+        //id
+        wd.findElement(By.cssSelector("#root")); // id = 'root'
+        wd.findElement(By.cssSelector("[id =\"root\"]")); //ili
+        // wd.findElement(By.cssSelector("[id="root"]")); - nel'zya!!!
+        wd.findElement(By.cssSelector("[id='root']"));
+
+        // На странице логин найти несколько элементов по их  attribute
+        wd.findElement(By.cssSelector("[href='/login']"));
+        wd.findElement(By.cssSelector("[href$='/login']"));
+
+        //На странице логин найти  элемент с  атрибутом placeholder, значение которого начинается на "Pass"
+        wd.findElement(By.cssSelector("[placeholder = 'Password']"));
+        wd.findElement(By.cssSelector("[placeholder ^= 'Pass']"));
+
+        //На странице логин найти  элемент с  атрибутом placeholder, значение которого  начинается на  "ass"
+        wd.findElement(By.cssSelector("[placeholder *= 'ass']"));
+
+        //На странице логин найти  элемент с  атрибутом placeholder, значение которого заканчивается на "ord"
+        wd.findElement(By.cssSelector("[placeholder $= 'ord']"));
+
+        // Объединение !!
+        //найти элемент div, у которого есть id со значением "root"  и класс со значением container
+        wd.findElement(By.cssSelector("div#root.container"));//ili
+        wd.findElement(By.cssSelector("div[id='root'][class ='container']"));
+
+        // найти элемент а, у которого есть атрибут href со значением /login
+        wd.findElement(By.cssSelector("a[href='/login'"));
+
+        // найти элемент input, у которого есть атрибут name cо значением email
+        wd.findElement(By.cssSelector("input[name='email']"));
+
+        // найти элемент button, у которого есть атрибут name cо значением login
+        wd.findElement(By.cssSelector("button[name='login']"));
+
+        // Поиск по дочерним элементам (* задание со звездоской)
+
+
+        // Найти кнопку регистрации, как последнего ребенка элемента form
+
+    }
+
 
    @AfterMethod
     public void postCondition()//close browser
